95c042f4394d7ef81d503d1aa624a19c77f72665
Arnab <you@example.com>
2020-09-12 04:39:33 +0600

    Banner Updated,background changed,clients part removed,some links added

---
+++ js/type.js
@@ -0,0 +1,7 @@
+$(".type-wrap span:last").typed({
+	strings: ["I am a Web Designer!","Developer!","I Can Create..","Awesome Stuff!","Let's Work Together."],
+    backDelay: 40,
+	typeSpeed: 150,
+	loop: true,
+	contentType: 'html'
+});
